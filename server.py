949bb9df603d154592648c500e5b3c6e4535dd91
Sundarraman R <144242629+Dev-Sundar@users.noreply.github.com>
2026-01-29 14:48:08 +0530

    server.py

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
-import uvicorn
-import numpy as np
+import pandas as pd
 
 app = FastAPI()
 
-# Load Model
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 model = joblib.load("stress_level_model_human_like.pkl")
 
-# Data Schema
-class SensorData(BaseModel):
+class SensorInput(BaseModel):
     ADXL345_X: float
     ADXL345_Y: float
     ADXL345_Z: float
@@ -20,20 +26,17 @@ class SensorData(BaseModel):
     GSR: float
 
 @app.post("/predict")
-def predict(data: SensorData):
+def predict(data: SensorInput):
+    df = pd.DataFrame([{
+        "ADXL345_X": data.ADXL345_X,
+        "ADXL345_Y": data.ADXL345_Y,
+        "ADXL345_Z": data.ADXL345_Z,
+        "MAX30102_HeartRate": data.HeartRate,
+        "MAX30102_SpO2": data.SpO2,
+        "BH1750_Light(lux)": data.LightLux,
+        "GSR_Value": data.GSR
+    }])
 
-    sample = np.array([[
-        data.ADXL345_X,
-        data.ADXL345_Y,
-        data.ADXL345_Z,
-        data.HeartRate,
-        data.SpO2,
-        data.LightLux,
-        data.GSR
-    ]])
+    pred = model.predict(df)[0]
 
-    pred = model.predict(sample)[0]
-
-    return { "stress_level": str(pred) }
-
-# Run server: uvicorn server:app --reload
+    return {"stress_level": pred}
